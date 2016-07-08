1d8a572851ccb9d7800e9b762b49d2b87d6df3d6
codeR <rabin.banerjee91@gmail.com>
2016-07-08 17:02:29 +0530

    Called scala from Java

--- src/main/java/MainJava.java
+++ src/main/java/MainJava.java
@@ -1,5 +1,7 @@
+import com.scala.HelloWorld;
 public class MainJava { 
     public static void main(String [ ] args) { 
+	HelloWorld.helloWorld();
         System.out.println("HELLOW WORLD FROM JAVA! "); 
     }
 }  
